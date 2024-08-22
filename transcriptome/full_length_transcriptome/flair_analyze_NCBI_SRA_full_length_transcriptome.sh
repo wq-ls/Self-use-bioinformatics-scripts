ac404548f7e489db984b06559fc3ae86132f5eb1
ls-wq <50230939+wq-ls@users.noreply.github.com>
2024-08-22 17:09:03 +0800

    Update flair_analyze_NCBI_SRA_full_length_transcriptome.sh

--- transcriptome/full_length_transcriptome/flair_analyze_NCBI_SRA_full_length_transcriptome.sh
+++ transcriptome/full_length_transcriptome/flair_analyze_NCBI_SRA_full_length_transcriptome.sh
@@ -12,4 +12,4 @@ flair align -r ${fq} -g ${genome} -t ${cpu} --junction_bed out_junction.bed
 flair correct -q flair.aligned.bed -g ${genome} --threads ${cpu} -f ${gtf}
 
 [ -d temp_flair ] || mkdir temp_flair
-flair collapse -r ${fq} -q flair_all_corrected.bed -g ${genome} -o flair.output --temp_dir temp_flair -t ${cpu} --keep_intermediate -f ${gtf} --isoformtss --no_gtf_end_adjustment --max_ends 5 --check_splice --longshot_bam flair.aligned.bam
+flair collapse -r ${fq} -q flair_all_corrected.bed -g ${genome} -o flair.output --temp_dir temp_flair -t ${cpu} --keep_intermediate -f ${gtf} --no_gtf_end_adjustment --max_ends 5 --check_splice --generate_map
