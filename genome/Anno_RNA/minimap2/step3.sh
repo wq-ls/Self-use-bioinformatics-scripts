dada4234ba785fd8f7a81ae7b74bd03da043ee31
ls-wq <50230939+wq-ls@users.noreply.github.com>
2024-09-04 20:58:58 +0800

    Delete genome/Anno_RNA/minimap2/step3.sh

--- genome/Anno_RNA/minimap2/step3.sh
+++
@@ -1,8 +0,0 @@
-
-cat pfam.qsub/pfam.1.domtblout > pfam.domtblout
-for i in `seq 2 200`
-do
-    less pfam.qsub/pfam.${i}.domtblout |grep -v '^#' >> pfam.domtblout
-done
-
-cat blast.qsub/*outfmt6 | awk '$3>60' > blastp.outfmt6
