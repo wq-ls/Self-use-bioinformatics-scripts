1e6052a1dcdb5c290456f270d6317f4312fb4db2
ls-wq <50230939+wq-ls@users.noreply.github.com>
2024-09-05 16:26:50 +0800

    Update step6.sh

--- Comparative_genomics/deal_tree_nwk/step6.sh
+++ Comparative_genomics/deal_tree_nwk/step6.sh
@@ -1,3 +1,3 @@
 ## rename leaves; TAB delimitor
-/dellfsqd2/ST_OCEAN/USER/lishuo1/00_tools/tree_deal.py --rename step6.old_new.change.txt raw.tree.nwk > step6.raw.tree.rename_Homo.nwk
-/dellfsqd2/ST_OCEAN/USER/lishuo1/00_tools/tree_deal.py -s step6.raw.tree.rename_Homo.nwk > step6.rename_Homo.txt
+tree_deal.py --rename step6.old_new.change.txt raw.tree.nwk > step6.raw.tree.rename_Homo.nwk
+ree_deal.py -s step6.raw.tree.rename_Homo.nwk > step6.rename_Homo.txt
