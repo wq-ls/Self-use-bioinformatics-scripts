bf49f3c3e75c594f6016bba7d43eca346a882f43
ls-wq <50230939+wq-ls@users.noreply.github.com>
2024-09-05 16:26:02 +0800

    Update step3.sh

--- Comparative_genomics/deal_tree_nwk/step3.sh
+++ Comparative_genomics/deal_tree_nwk/step3.sh
@@ -1,7 +1,7 @@
 ## sort the tree node
 ## 0 for decreasing     1 for increasing
 
-/dellfsqd2/ST_OCEAN/USER/lishuo1/00_tools/tree_deal.py -l 0 raw.tree.nwk > step3.raw.tree.sort.decreasing.nwk
-/dellfsqd2/ST_OCEAN/USER/lishuo1/00_tools/tree_deal.py -l 1 raw.tree.nwk > step3.raw.tree.sort.increasing.nwk
-/dellfsqd2/ST_OCEAN/USER/lishuo1/00_tools/tree_deal.py -s step3.raw.tree.sort.increasing.nwk > step3.increasing.txt
-/dellfsqd2/ST_OCEAN/USER/lishuo1/00_tools/tree_deal.py -s step3.raw.tree.sort.decreasing.nwk > step3.decreasing.txt
+tree_deal.py -l 0 raw.tree.nwk > step3.raw.tree.sort.decreasing.nwk
+tree_deal.py -l 1 raw.tree.nwk > step3.raw.tree.sort.increasing.nwk
+tree_deal.py -s step3.raw.tree.sort.increasing.nwk > step3.increasing.txt
+tree_deal.py -s step3.raw.tree.sort.decreasing.nwk > step3.decreasing.txt
