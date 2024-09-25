83f8e1fe4adee9d3786419d0747f38a5bfad7f0d
ls-wq <50230939+wq-ls@users.noreply.github.com>
2024-09-25 11:03:27 +0800

    Update split_haphic_step1.sh

--- genome/Hic/haphic/split_haphic_step1.sh
+++ genome/Hic/haphic/split_haphic_step1.sh
@@ -5,5 +5,8 @@ micromamba activate haphic
 genome=out_correct.fa
 cpu=80
 nchrs=23
+inflation_step=0.1
+min_inflation=1
+max_inflation=1.5
 
-haphic cluster --remove_concentrated_links --remove_allelic_links 2 --threads ${cpu} --correct_nrounds 2 --correct_resolution 250 ${genome} HiC.filtered.bam ${nchrs}
+haphic cluster --remove_concentrated_links --remove_allelic_links 2 --threads ${cpu} --correct_nrounds 2 --correct_resolution 250 ${genome} HiC.filtered.bam ${nchrs} --inflation_step ${inflation_step} --min_inflation ${min_inflation}  --max_inflation ${max_inflation}
