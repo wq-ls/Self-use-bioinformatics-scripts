a9040eb1bb804f8d4890d84cd3c4730a307b4998
ls-wq <50230939+wq-ls@users.noreply.github.com>
2024-09-25 11:04:51 +0800

    Update split_haphic_step2.sh

--- genome/Hic/haphic/split_haphic_step2.sh
+++ genome/Hic/haphic/split_haphic_step2.sh
@@ -6,5 +6,7 @@ genome=corrected_asm.fa
 cpu=80
 nchrs=23
 best_inflation=1.5
+ambiguous_cutoff=0.4
+remove_allelic_links=2
 
-haphic reassign --ambiguous_cutoff 0.4 --remove_allelic_links 2 --nclusters ${nchrs} --threads ${cpu} ${genome} full_links.pkl inflation_${best_inflation}/mcl_inflation_${best_inflation}.clusters.txt paired_links.clm
+haphic reassign --ambiguous_cutoff ${ambiguous_cutoff} --remove_allelic_links ${remove_allelic_links} --nclusters ${nchrs} --threads ${cpu} ${genome} full_links.pkl inflation_${best_inflation}/mcl_inflation_${best_inflation}.clusters.txt paired_links.clm
