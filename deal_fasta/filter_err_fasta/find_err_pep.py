8e1a52b1643579b6995bff2050cecfadbdf399a4
ls-wq <50230939+wq-ls@users.noreply.github.com>
2024-09-26 10:18:42 +0800

    Create find_err_pep.py

---
+++ deal_fasta/filter_err_fasta/find_err_pep.py
@@ -0,0 +1,25 @@
+
+import sys
+from Bio import SeqIO
+
+def check_protein_sequences(fasta_file):
+    # Define legal protein character sets
+    valid_chars = set("ACDEFGHIKLMNPQRSTVWYacdefghiklmnpqrstvwy")
+
+    with open(fasta_file, "r") as file:
+        for record in SeqIO.parse(file, "fasta"):
+            sequence = str(record.seq)
+            invalid_chars = [char for char in sequence if char not in valid_chars]
+            if invalid_chars:
+                print(f">{record.id}")
+                print(f"{sequence}")
+#                print(f"Illegal_character: {set(invalid_chars)}")
+#                print("-" * 40)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <path_to_fasta_file>")
+        sys.exit(1)
+
+    fasta_file_path = sys.argv[1]
+    check_protein_sequences(fasta_file_path)
