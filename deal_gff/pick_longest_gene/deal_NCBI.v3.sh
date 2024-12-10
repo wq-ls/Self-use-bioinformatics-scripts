c6c56aa049c0690465603f164df01436c9a15cee
ls-wq <50230939+wq-ls@users.noreply.github.com>
2024-12-10 15:52:10 +0800

    Update deal_NCBI.v3.sh

--- deal_gff/pick_longest_gene/deal_NCBI.v3.sh
+++ deal_gff/pick_longest_gene/deal_NCBI.v3.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 set +o posix
 
-species=S_sala
-NCBI_gff=genomic.gff
-pep=protein.faa
-cds=GCF_905237065.1_Ssal_v3.1_cds_from_genomic.fna
-feature=GCF_905237065.1_Ssal_v3.1_feature_table.txt
+## test data get from https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/972/845/GCF_000972845.2_L_crocea_2.0/
+species=O_nilo
+NCBI_gff=GCF_000972845.2_L_crocea_2.0_genomic.gff
+pep=GCF_000972845.2_L_crocea_2.0_protein.faa
+cds=GCF_000972845.2_L_crocea_2.0_cds_from_genomic.fna
+feature=GCF_001858045.2_O_niloticus_UMD_NMBU_feature_table.txt
+gaf=GCF_001858045.2_O_niloticus_UMD_NMBU_gene_ontology.gaf
 
 ## get longest and contain UTR gff file
 grep -e "gene_biotype=protein_coding" -e "gbkey=mRNA;gene" -e "protein_id=" ${NCBI_gff} > pep.gff
-/usr/bin/singularity run --bind $PWD:$PWD /01_soft/singularity_all/Agata.sif agat_sp_keep_longest_isoform.pl -gff pep.gff -o ${species}.UTR.gff
+/usr/bin/singularity run --bind $PWD:$PWD /dellfsqd2/ST_OCEAN/USER/lishuo11/01_soft/singularity_all/Agata.sif agat_sp_keep_longest_isoform.pl -gff pep.gff -o ${species}.UTR.gff
 rm pep.gff pep.agat.log
 
 ## get Gene XP XM symbol description
 awk '$3=="gene"' ${species}.UTR.gff | cut -f 9 | sed 's/;/\t/g' | sed 's/ID=gene-//1;s/Dbxref=GeneID://1;s/Name=//1' | cut -f 2,3 > geneID_name
 awk '$3=="CDS"' ${species}.UTR.gff | cut -f 9 | sed 's/;/\t/g' | sed 's/ID=cds-//1;s/Parent=rna-//1' | sed 's/,/\t/1' | awk '{if ($3~/GeneID/) print $3"\t"$1"\t"$2; else print $4"\t"$1"\t"$2}' | sed 's/Dbxref=GeneID://g' | sed 's/GeneID://g' | awk -F "\t" 'NR==FNR{a[$1]=$2}NR!=FNR{print $0"\t"a[$1]}' geneID_name - | awk '!a[$0]++' > tmp1
 awk '{print $2"\t"$0}' tmp1 | awk -F "\t" 'NR==FNR{a[$1]=$2}NR!=FNR{print $0"\t"a[$1]}' <(grep ">" ${pep} | sed 's/>//1;s/ /\t/1;s/\[/\t/1' | cut -f 1,2) - | cut -f 2- > NCBI.gene.match.txt
+sed 's/ /||||||||/g' NCBI.gene.match.txt | csvtk -t add-header -n GeneID,pepID,rnaID,Symbol,description | csvtk join -t --left-join --na NA - <(sed 's/\!\#DB/DB/g' ${gaf} | grep -v "^\!" | cut -f 2-) | sed 's/||||||||/ /g' > NCBI.gene.match.more.txt
 rm tmp1 geneID_name
 
 ## get simple gff cds pep
