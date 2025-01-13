a54d5a0b7eb198eb99c563e80d91e631eb57d14b
ls-wq <50230939+wq-ls@users.noreply.github.com>
2025-01-13 18:58:39 +0800

    Update check_species_by_mit_pep.sh

--- genome/Mit_genome/check_species_by_mit_pep.sh
+++ genome/Mit_genome/check_species_by_mit_pep.sh
@@ -14,3 +14,5 @@ database=mitochondrion
 # diamond makedb --in mitochondrion.1.protein.faa -d mitochondrion --taxonnodes nodes.dmp --taxonnames names.dmp --taxonmap prot.accession2taxid.gz
 
 diamond blastp -d ${database} -q ${input_mit_pep} -o ${input_mit_pep}.guess_sp.txt -p ${cpu} --ultra-sensitive --evalue ${evalue} --quiet --matrix ${matrix} --masking 1 --comp-based-stats 1 --max-hsps 0 --query-cover ${query_cover} --subject-cover ${subject_cover} --outfmt 6 qseqid sseqid evalue pident staxids sscinames sphylums
+
+sort -k 1,1 -k 4nr,4 ${input_mit_pep}.guess_sp.txt | awk '!a[$1]++{print $0}' > ${input_mit_pep}.guess_sp2.txt
